0d7fedf8901590d48072cd7f869f4bcea1dad1d5
GalaxyGamingBoy <xrteach@gmail.com>
2022-07-09 12:13:56 +0000

    Added Tests Passed

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import filecmp
 
 if sys.platform == "linux" or sys.platform == "linux2":
     os.system("bash ./Scripts/tests.sh")
@@ -7,3 +8,8 @@ elif sys.platform == "darwin":
     os.system("sh ./Scripts/tests.sh")
 elif sys.platform == "win32":
     os.system(".\\Scripts\\tests.bat")
+
+if filecmp.cmp('./tests.log', './Tests/tests.passed'):
+    print("Tests Passed")
+else:
+    print("Tests Failed, See tests.log")
